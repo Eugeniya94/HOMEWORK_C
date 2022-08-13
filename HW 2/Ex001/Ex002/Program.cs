18693808071bff8a1007d6d69c3d535ee4891014
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-13 17:56:10 +0300

    change ex002

--- HW 2/Ex001/Ex002/Program.cs
+++ HW 2/Ex001/Ex002/Program.cs
@@ -4,3 +4,24 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.Write("Ведите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstnum = number/100;
+
+int Endnumber(int num)
+{
+    while(num>=100 && num<=1000)
+    {
+        int firstnum = num/100;
+    }
+        if(firstnum % 100 == 0) return -1;
+        int result = firstnum % 10;
+        
+    return result;
+    }
+
+int endnumber = Endnumber(number);
+Console.WriteLine(endnumber);
+
+
