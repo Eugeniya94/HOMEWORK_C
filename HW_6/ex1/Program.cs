7c4910e129a3b86828189bf9eeefbff8015ae6e2
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-28 18:23:03 +0300

    add ex1

---
+++ HW_6/ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿// // Задача 41: Пользователь вводит с клавиатуры M чисел.
+// // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write($"Введите количество чисел, которое вы собираетесь ввести: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[number];
+
+void InpNumbers(int num)
+{
+for (int i = 0; i < num; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int OverZeroNum(int[] arrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < arrayNumbers.Length; i++)
+  {
+    if(arrayNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InpNumbers(number);
+
+Console.WriteLine($"Количество чисел больше 0: {OverZeroNum(arrayNumbers)} ");
